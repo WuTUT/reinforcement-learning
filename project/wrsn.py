ba8f2238951c7e19f4a44cc95341a28434070df4
WuTUT <762641171@qq.com>
2019-04-01 22:10:09 +0800

    uuu

--- project/wrsn.py
+++ project/wrsn.py
@@ -3,39 +3,63 @@ import gym
 from gym import spaces
 
 #define constant value
-dtheta=0.5
-k1=0.2
-k2=3
-modulation_level=5
-queue_len=6
-battle_level=5
-packet_payload=256
-ber_requirement=0.0005
-T_len=5
-data_arrival_p=
+
+queue_len=20
+battle_level=40
+
+distance_level=5
+sensor_node=10
+B=np.random.randint(1,battle_level,sensor_node)
+D=np.random.randint(0,10,sensor_node)
+H=np.random.randint(0,5,sensor_node)
+data_prob=np.ones(sensor_node)*0.1
+def funEh(distance):
+    Eh=[5,5,4,4,3]
+    return Eh[distance]
+
+def funPh(distance):
+    Ph=[1,1,2,2,3]
+    return Ph[distance]    
+
 def calculate_reward():
     return reward
-def calculate_modulationK():
-    return modulationK
-def calculate_incbattery(modulationK)
-def calculate_trans_P(idnode,modulationK,):
+
+def calculate_incbattery()
+def calculate_trans_P():
     return 
 
-class WrsnEnv(gym.Env):
-    def __init__(self,sensor_node=40):
+#Sensor Node select Env
+class nsEnv(gym.Env):
+    def __init__(self,sensor_node):
         self.action_space = spaces.Discrete(sensor_node)
-        self.observation_space = spaces.Tuple((((spaces.Discrete(battle_level),spaces.Discrete(queue_len+1)),)*sensor_node))
+        self.observation_space = spaces.Tuple((((spaces.Discrete(battle_level+1),spaces.Discrete(queue_len+1)),)*sensor_node))
+        
+
     def step(self,action):
         done=False
         reward=0.0
         #TO DO
-
+    
         return 
     def _get_obs(self):
 
     def reset():
 
         return self._get_obs()
+
+#switch EH or DT Env
+class chEnv(gym.Env):
+    def __init__(self):
+        self.action_space = spaces.Discrete(2)
+        self.observation_space=spaces.Tuple((spaces.Discrete(battle_level+1),spaces.Discrete(queue_len+1),spaces.Discrete(distance_level+1)))
+
+    def step(self,action):
+        done=False
+        reward=0.0
+
+
+
+
 
 
 
